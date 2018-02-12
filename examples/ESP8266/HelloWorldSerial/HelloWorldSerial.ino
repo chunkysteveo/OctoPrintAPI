0937cb92f576b130792ba5620202716690cd8a77
witnessmenow <ecenuig@gmail.com>
2018-02-12 23:30:16 +0000

    Updating one example, not even testsed!

--- examples/ESP8266/HelloWorldSerial/HelloWorldSerial.ino
+++ examples/ESP8266/HelloWorldSerial/HelloWorldSerial.ino
@@ -1,29 +1,30 @@
 /*******************************************************************
- *  A "Hello World" sketch to make sure your Arduino and your 
- *  OctoPrint server can speak to each other. This will show your 
- *  OctoPrint Server, API and 3D printer statistics from the 
+ *  A "Hello World" sketch to make sure your Arduino and your
+ *  OctoPrint server can speak to each other. This will show your
+ *  OctoPrint Server, API and 3D printer statistics from the
  *  OctoPrint API.
- * 
+ *
  *  You will need the IP or hostname of your OctoPrint server, a
  *  port number (will be 80 unless you are reaching it from an
- *  external source) and an API key from the OctoPrint 
+ *  external source) and an API key from the OctoPrint
  *  installation - http://docs.octoprint.org/en/master/api/general.html#authorization                              *
  *  You will also need to enable CORS - http://docs.octoprint.org/en/master/api/general.html#cross-origin-requests
  *                                                                 *
- *  By Stephen Ludgate https://www.youtube.com/channel/UCVEEuAouZ6ua4oetLjjHAuw 
+ *  By Stephen Ludgate https://www.youtube.com/channel/UCVEEuAouZ6ua4oetLjjHAuw
  *******************************************************************/
 
 
 #include <OctoPrintAPI.h> //This is where the magic happens... shazam!
 
-#include <ESP8266WiFi.h>  
+#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
- 
+
 const char* ssid = "SSID";          // your network SSID (name)
 const char* password = "PASSWORD";  // your network password
 
 const String octoprint_host = "IP_ADDRESS";  //Your internal IP address of your OctoPrint server, or your extenral hostname/IP if you are external
+IPAddress ip(192, 168, 1, 5);
 const int octoprint_httpPort = 80;  //If you are connecting through a router this will work, but you need a random port forwarded to the OctoPrint server from your router. Enter that port here if you are external
 String octoprint_apikey = "API_KEY"; //See top of file or GIT Readme about getting API key
 
@@ -37,12 +38,12 @@ String printerTarget;
 String payload;
 
 
-OctoprintApi api(octoprint_host, octoprint_httpPort, octoprint_apikey);
+OctoprintApi api(client, ip, octoprint_httpPort, octoprint_apikey);
 
 unsigned long api_mtbs = 60000; //mean time between api requests (60 seconds)
 unsigned long api_lasttime = 0;   //last time api request has been done
 
-void setup () { 
+void setup () {
   Serial.begin(115200);
   delay(10);
 
@@ -51,13 +52,13 @@ void setup () {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -65,16 +66,16 @@ void setup () {
 
   //if you get here you have connected to the WiFi
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP()); 
+  Serial.println(WiFi.localIP());
 }
 
 
 void loop() {
 
   if (millis() - api_lasttime > api_mtbs || api_lasttime==0) {  //Check if time has expired to go check OctoPrint
-    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status     
+    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
       if(api.getOctoprintVersion()){
         Serial.println("---------Version---------");
         Serial.print("Octoprint API: ");
@@ -102,7 +103,7 @@ void loop() {
         Serial.println(api.printerStats.printerStateready);
         Serial.print("Printer State - sdReady:  ");
         Serial.println(api.printerStats.printerStatesdReady);
-        Serial.println("------------------------");              
+        Serial.println("------------------------");
       }
     }
     api_lasttime = millis();  //Set api_lasttime to current milliseconds run
